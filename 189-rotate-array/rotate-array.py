e5fa75632023e8cc80b779c526fcd955dd74401e
guruprasad06 <mguruprasad2006@gmail.com>
2026-09-01 11:32:16 +0530

    Time: 147 ms (12.97%) | Memory: 35.5 MB (8.15%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotate(self, nums: list[int], k: int) -> None:
+        def rev(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+
+        k = k % len(nums)
+        rev(0, len(nums) - 1)
+        rev(0, k - 1)
+        rev(k, len(nums) - 1)
