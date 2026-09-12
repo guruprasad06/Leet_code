7c5910aceaaabfd6268c173f04e339ae7300149b
guruprasad06 <mguruprasad2006@gmail.com>
2026-09-12 20:27:13 +0530

    Time: 6 ms (43.90%) | Memory: 20.5 MB (25.26%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,10 +3,11 @@ class Solution:
         c=nums.count(0)
         k=[]
         for i in nums:
-            if i != 0:
+            if i!=0:
                 k.append(i)
-        
         for i in range(c):
             k.append(0)
         for i in range(len(nums)):
             nums[i]=k[i]
+
+        
