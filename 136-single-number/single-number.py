300f4f65bb49191fb9250dc735bb841b1b0f83c1
guruprasad06 <mguruprasad2006@gmail.com>
2026-09-13 17:49:55 +0530

    Time: 7 ms (29.42%) | Memory: 21.8 MB (10.67%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,6 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        sum=nums[0]
-        for i in range(1,len(nums)):
-            sum^=nums[i]
-        return sum
+        d={}
+
+        for i in nums:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+        for i in d:
+            if d[i]==1:
+                return i
