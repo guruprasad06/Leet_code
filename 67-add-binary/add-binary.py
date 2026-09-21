f5a123c9d9f94d4a8964341815f50a73e2f9cfb2
guruprasad06 <mguruprasad2006@gmail.com>
2026-09-21 22:41:01 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (85.38%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -2,7 +2,8 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         i=int(a,2)
         j=int(b,2)
-        s=i+j
-        res=bin(s)
+        Sum=i+j
+        print(Sum)
+        res=bin(Sum)
         print(res)
         return res[2:]
