4f64e1fc1fe75e89e34a9500858428697205a00c
guruprasad06 <mguruprasad2006@gmail.com>
2026-09-22 19:42:42 +0530

    Time: 65 ms (68.86%) | Memory: 19.7 MB (59.92%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -4,6 +4,7 @@ class Solution:
         for i in s:
             j=ord(i)-97
             l[j]+=1
+        print(l)
         for i in range(len(s)):
             if l[ord(s[i])-97]==1:
                 return i
