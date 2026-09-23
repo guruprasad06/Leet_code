4e1eddf7b2f02c1496e9b4a803515a47450bc396
guruprasad06 <mguruprasad2006@gmail.com>
2026-09-23 21:44:28 +0530

    Time: 3 ms (51.12%) | Memory: 19.1 MB (91.22%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,9 +1,9 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s=sorted(s)
-        t=sorted(t)
         i=0
         j=0
+        s=sorted(s)
+        t=sorted(t)
 
         while i<len(s) and j<len(t):
             if s[i]!=t[j]:
@@ -11,3 +11,4 @@ class Solution:
             i+=1
             j+=1
         return t[j]
+            
