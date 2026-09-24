2586a7c306749f733617788faa1dcecc41817eaf
guruprasad06 <mguruprasad2006@gmail.com>
2026-09-24 13:49:34 +0530

    Time: 7 ms (87.57%) | Memory: 22.2 MB (5.22%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -3,15 +3,14 @@ class Solution:
         s=list(s)
         l=[]
         j=[]
+
         for i in range(len(s)):
             if s[i] in 'aeiouAEIOU':
                 l.append(s[i])
                 j.append(i)
-        print(l)
         l=l[::-1]
         print(l)
+
         for i in range(len(l)):
             s[j[i]]=l[i]
         return "".join(s)
-            
-
